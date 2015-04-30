112f6b77a42e2c5c9706632c4df0842dd4fd0f42
russell goldenberg <russellgoldenberg@gmail.com>
2015-04-30 09:39:47 -0400

    optimal image size

--- src/js/main.js
+++ src/js/main.js
@@ -23,13 +23,16 @@
 
     var displayStories = function(data) {
         var num = data.length;
+        var threshold = 640;
+        var width = window.innerWidth * window.devicePixelRatio;
+        var biggerImage = width > threshold;
+
         if(data.length % 2 === 1) {
             num -=1;
         }
 
         for(var x = 0; x < num; x++ ) {
             var story = data[x];
-            // var hed = parseTitle(story.hed); 
             var description = shortenDescription(story.description);
 
             var html = '<a href="' + story.url + '">';
@@ -84,7 +87,9 @@
             if(replaceWithLogo(story.image)) {
                 story.image = 'img/bg.jpg';
             } else {
-                story.image = story.image.replace('585w', '835w');
+                if(biggerImage) {
+                    story.image = story.image.replace('585w', '835w');    
+                }
             }
             img.src = story.image;     
         };
@@ -115,17 +120,6 @@
         el.innerText = 'Last updated: ' + updated.time;
     };
 
-    // var parseTitle = function(str) {
-    //     var split = str.split('-');
-    //     if(split.length > 2) {
-    //         //if last is blank, it ran out of room, connect everything before that
-    //         split = split.slice(0, split.length - 2);
-    //     } else if(split.length === 2) {
-    //         split = split.slice(0, 1);
-    //     }
-    //     return split.join('-');
-    // };
-
     var shortenDescription = function(str) {
         var max = 240;
         if(str.length > max) {
@@ -136,6 +130,17 @@
             return str;
         }
     };
+
+    // var parseTitle = function(str) {
+    //     var split = str.split('-');
+    //     if(split.length > 2) {
+    //         //if last is blank, it ran out of room, connect everything before that
+    //         split = split.slice(0, split.length - 2);
+    //     } else if(split.length === 2) {
+    //         split = split.slice(0, 1);
+    //     }
+    //     return split.join('-');
+    // };
 
     init();
 })();
